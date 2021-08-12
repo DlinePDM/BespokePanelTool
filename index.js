fde17d2e8d52a1b4be45b63a88547b2c8d6d6449
Gytautas Balevicius <gba@dline.com>
2021-08-12 11:52:23 +0300

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 import {GLTFLoader} from 'https://unpkg.com/three@0.126.0/examples/jsm/loaders/GLTFLoader.js'
 import {OrbitControls} from 'https://unpkg.com/three@0.126.0/examples/jsm/controls/OrbitControls.js'
+import {GLTFExporter} from 'https://unpkg.com/three@0.126.0/examples/jsm/exporters/GLTFExporter.js'
 
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
+
+export function DownloadMDL(){
+    console.log("yes")
+}
+
+
 
 const loader = new GLTFLoader()
 loader.load('models/example.glb', function(glb){
@@ -11,24 +18,40 @@ loader.load('models/example.glb', function(glb){
     const root = glb.scene;
     root.scale.set(0.005,0.005,0.005)
     scene.add(root);
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total*100)+"% loaded")
-}, function(error){
-    console.log('an error occured')
+    }, function(xhr){
+        console.log((xhr.loaded/xhr.total*100)+"% loaded")
+    }, function(error){
+        console.log('an error occured');
 })
 
-const loader2 = new GLTFLoader()
-loader2.load('models/example.glb', function(glb){
-    console.log(glb)
-    const root = glb.scene;
-    root.scale.set(0.005,0.005,0.005)
-    root.position.set(0,-2.25,0)
-    scene.add(root);
-}, function(xhr){
-    console.log((xhr.loaded/xhr.total*100)+"% loaded")
-}, function(error){
-    console.log('an error occured')
-})
+const btn = document.getElementById('btnDWN')
+btn.addEventListener('click', download)
+
+function download() {
+    const exporter = new GLTFExporter()
+        exporter.parse(
+            scene,
+            function(result){
+                saveArrayBuffer(result, 'test.glb')
+            },
+        {
+            binary:true
+        }
+        )
+}
+
+function saveArrayBuffer(buffer, fileName){
+    save(new Blob([buffer], {type: 'application/octet-stream'}), fileName)
+}
+
+const link = document.createElement('a')
+document.body.appendChild(link)
+
+function save(blob, fileName){
+    link.href = URL.createObjectURL(blob)
+    link.download = fileName
+    link.click()
+}
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2,2,2)
@@ -63,4 +86,4 @@ function animate(){
     renderer.render(scene,camera);
 }
 
-animate()
+animate();
